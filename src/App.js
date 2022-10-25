028290ad91c934dc0aae60596d6aa6ac976ce548
hildelio <hildelio@gmail.com>
2022-10-25 14:42:43 -0300

    Requisito 2

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Pokedex from './components/Pokedex';
 import pokemonList from './data';
 
-class App extends React.Component {
-  // state = {
-  //   list: pokemonList,
-  // }
-  // handleChangeList = () => {
-  //   this.setState((prevState) => ({ 
-  //     list: prevState[0], 
-  //   }));
-  // };
+class App extends Component {
+  state = {
+    index: 0,
+  };
+
+  handleNextPokemon = () => {
+    this.setState((prevState) => ({
+      index: prevState.index + 1,
+    }));
+  };
+
   render() {
+    const { index } = this.state;
     return (
       <div className="App">
-        <Pokedex pokemonList={ pokemonList[0] } />
+        <Pokedex pokemonList={ pokemonList[index] } />
+        <button
+          type="button"
+          onClick={ () => this.handleNextPokemon(pokemonList.length) }
+        >
+          Próximo pokémon
+        </button>
       </div>
     );
   }
